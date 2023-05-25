e69816b87bbb11183a0b2c27b820bec6ffaefb06
StacyFranco <stacyfranco@hotmail.com>
2023-05-25 19:33:49 -0300

    Ajusting App for the Jammming

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+//import Track from './components/Track/Track';
+//import Tracklist from './components/Tracklist/Tracklist';
+import SearchBar from './components/SearchBar/SearchBar';
+import SearchResults from './components/SearchResults/SearchResults';
+import Playlist from './components/Playlist/Playlist';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jammming</h1>
+        
+        
       </header>
+        <SearchBar />
+      <main className="App-main">
+        <SearchResults />
+        <Playlist />
+      </main>
+      <footer className="App-footer">
+        <p>copyright</p>
+        <p><a
+          className="App-link"
+          href="https://www.freepik.com/free-vector/abstract-music-background_1250515.htm#query=music%20background&position=43&from_view=search&track=ais"
+          >Image by Harryarts</a> on Freepik</p>
+      </footer>
     </div>
   );
 }
